7e6056e510b71d267c79c38830ed3e3a0ad3da85
Denis Suprun <yasuprundenis@yandex.ru>
2023-09-16 19:35:48 +0300

    + Cleaning some imports + Minor fixes

--- src/startcoordinator.h
+++ src/startcoordinator.h
@@ -1,65 +1,55 @@
 #ifndef STARTCOORDINATOR_H
 #define STARTCOORDINATOR_H
 
-#include "easy_import.h"
-
-#include <QObject>
-#include <QtQuick>
-
 #include "smoozyutils.h"
 #include "pagepaths.h"
 
-#include "startvm.h"
-#include "loginvm.h"
-#include "editprofilevm.h"
-
-#include "ilogintokenprovider.h"
 #include "istartdiprovider.h"
 
 class StartCoordinator : public QObject
 {
     Q_OBJECT
 
-    shared_ptr<QQuickItem> pageStackCppWrapper;
-    shared_ptr<IStartDiProvider> diProvider;
+    shared_ptr<QQuickItem> m_pageStackCppWrapper;
+    shared_ptr<IStartDiProvider> m_diProvider;
     shared_ptr<StartService> m_startService;
 
 public:
     explicit StartCoordinator(shared_ptr<QQuickItem> pageStackCppWrapper, shared_ptr<IStartDiProvider> diProvider, QObject *parent = nullptr)
         : QObject(parent)
-        , pageStackCppWrapper { pageStackCppWrapper }
-        , diProvider { diProvider }
+        , m_pageStackCppWrapper { pageStackCppWrapper }
+        , m_diProvider { diProvider }
         , m_startService { diProvider->startServiceInstance() }
     { qDebug(); };
 
     ~StartCoordinator() { qDebug(); };
 
     void start(bool isReplace = false) {
-        auto vm = unique_unwrap(diProvider->startVmInstance());
+        auto vm = unique_unwrap(m_diProvider->startVmInstance());
 
         QObject::connect(vm, &StartVM::login, this, &StartCoordinator::goToLogin);
         QObject::connect(vm, &StartVM::signup, this, &StartCoordinator::goSignup);
 
         if (isReplace) {
-            Smoozy::replaceAllWithNamedPage(pageStackCppWrapper.get(), PagePaths::startPage, vm);
+            Smoozy::replaceAllWithNamedPage(m_pageStackCppWrapper.get(), PagePaths::startPage, vm);
         } else {
-            Smoozy::pushNamedPage(pageStackCppWrapper.get(), PagePaths::startPage, vm);
+            Smoozy::pushNamedPage(m_pageStackCppWrapper.get(), PagePaths::startPage, vm);
         }
     };
 
 public slots:
     void goToLogin() {
-        auto vm = unique_unwrap(diProvider->loginVmInstance(m_startService));
+        auto vm = unique_unwrap(m_diProvider->loginVmInstance(m_startService));
         QObject::connect(vm, &LoginVM::authorized, this, &StartCoordinator::authDone);
 
-        Smoozy::pushNamedPage(pageStackCppWrapper.get(), PagePaths::loginPage, vm);
+        Smoozy::pushNamedPage(m_pageStackCppWrapper.get(), PagePaths::loginPage, vm);
     };
 
     void goSignup() {
-        auto vm = unique_unwrap(diProvider->editProfileInstance(m_startService));
+        auto vm = unique_unwrap(m_diProvider->editProfileInstance(m_startService));
         QObject::connect(vm, &EditProfileVM::authorized, this, &StartCoordinator::authDone);
 
-        Smoozy::pushNamedPage(pageStackCppWrapper.get(), PagePaths::editProfilePage, vm);
+        Smoozy::pushNamedPage(m_pageStackCppWrapper.get(), PagePaths::editProfilePage, vm);
     };
 
     void authDone() { emit authorized(); };
